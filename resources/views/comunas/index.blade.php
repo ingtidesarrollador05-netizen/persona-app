9ef4de1558a08265a7976207157f83f414c32a08
santiago <usuario@dominio.com>
2026-03-12 15:28:48 -0500

    Comuna list

---
+++ resources/views/comunas/index.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Listado de Columnas</title>
+  </head>
+  
+
+  <body>
+    <div class="container">
+        <h1>Comune List</h1>
+        <a href="{{route ('comunas.create') }}" class="btn btn-success">Add</a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Code</th>
+                    <th scope="col">Commune</th>
+                    <th scope="col">Municipality</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($comunas as $comuna)
+                <tr>
+                    <th scope= "row">{{ $comuna->comu_codi }}</th>
+                    <td>{{ $comuna->comu_nomb }}</td>
+                    <td>{{ $comuna->muni_nomb }}</td>
+                    <td><span> Actions </span></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+
+    <table class="table">
+        <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+    <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    </tbody>
+    </table>
+  </body>
+</html>
